a45806118df51f17d8270cb76d932fb779b0d152
akshay-0223 <125637054+akshay-0223@users.noreply.github.com>
2023-02-17 12:21:24 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DevTown</h1>
+      <p>
+        We at ShapeAI (A research and education-based startup ), are a team with
+        an aim to provide students with a platform to research and make projects
+        in the field of AI and ML. We aim to provide an interactive learning
+        platform for students where they will be able to learn and develop
+        projects with our support. We have collaborated with Kesha Kaneria,
+        Microsoft Learn Student Ambassador(Beta Level) and no charge will be
+        taken for the events or webinars conducted and certified by them.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
